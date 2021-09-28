60ef1e4b38716ea6fb7ab838e14b6954115fd748
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-09-28 22:43:08 +0200

    Bola extra de la primera clase de javascript

--- balance/forms.py
+++ balance/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, StringField, FloatField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms import HiddenField, DateField, StringField, FloatField, RadioField, SubmitField
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+
+import datetime
+
+def validar_fecha(formulario, campo):
+    hoy = datetime.date.today()
+    if campo.data > hoy:
+        raise ValidationError("La fecha no puede ser posterior a hoy - soy externo")
 
 class MovimientoFormulario(FlaskForm):
+
+    id = HiddenField()
+
     fecha = DateField("Fecha", validators=[DataRequired(message="Debe informar la fecha")])
     concepto = StringField("Concepto", validators=[DataRequired(message="Debe informar el concepto"), Length(min=10)])
     cantidad = FloatField("Cantidad", validators=[DataRequired(message="Debe  informar el monto del movimiento"), 
@@ -11,3 +21,8 @@ class MovimientoFormulario(FlaskForm):
                                             choices=[('G', 'Gasto'), ('I', 'Ingreso')])
 
     submit = SubmitField('Aceptar')
+
+    def validate_fecha(self, campo):
+        hoy = datetime.date.today()
+        if campo.data > hoy:
+            raise ValidationError("La fecha no puede ser posterior a hoy - interno")
